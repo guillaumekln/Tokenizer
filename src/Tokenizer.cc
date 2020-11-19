725456a474fa5bc726378e434baf68a717dd38bf
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-11-19 14:00:28 +0100

    Remove one indentation level

--- src/Tokenizer.cc
+++ src/Tokenizer.cc
@@ -602,250 +602,248 @@ namespace onmt
     // TODO: this method has grown big and is hard to follow. It should be refactored into
     // smaller pieces to clarify its logic.
 
+    std::vector<std::string> chars;
+    std::vector<unicode::code_point_t> code_points_main;
+    std::vector<std::vector<unicode::code_point_t>> code_points_combining;
+    unicode::explode_utf8_with_marks(text,
+                                     chars,
+                                     &code_points_main,
+                                     &code_points_combining,
+                                     &exclude_combining);
+
+    std::vector<unicode::CharType> char_types;
+    char_types.reserve(code_points_main.size());
+    for (const auto code_point : code_points_main)
+      char_types.emplace_back(unicode::get_char_type(code_point));
+
+    Token token;
+    int state = State::Space;
+    int prev_alphabet = -1;
+
+    for (size_t i = 0; i < chars.size(); ++i)
     {
-      std::vector<std::string> chars;
-      std::vector<unicode::code_point_t> code_points_main;
-      std::vector<std::vector<unicode::code_point_t>> code_points_combining;
-      unicode::explode_utf8_with_marks(text,
-                                       chars,
-                                       &code_points_main,
-                                       &code_points_combining,
-                                       &exclude_combining);
+      const bool letter = state & State::Letter;
+      const bool space = state & State::Space;
+      const bool number = state & State::Number;
+      const bool other = state & State::Other;
+      const bool placeholder = state & State::Placeholder;
 
-      std::vector<unicode::CharType> char_types;
-      char_types.reserve(code_points_main.size());
-      for (const auto code_point : code_points_main)
-        char_types.emplace_back(unicode::get_char_type(code_point));
-
-      Token token;
-      int state = State::Space;
-      int prev_alphabet = -1;
-
-      for (size_t i = 0; i < chars.size(); ++i)
+      const std::string& c = chars[i];
+      if (_options.support_prior_joiners && c == _options.joiner)
       {
-        const bool letter = state & State::Letter;
-        const bool space = state & State::Space;
-        const bool number = state & State::Number;
-        const bool other = state & State::Other;
-        const bool placeholder = state & State::Placeholder;
-
-        const std::string& c = chars[i];
-        if (_options.support_prior_joiners && c == _options.joiner)
+        /* it is either after a space, in that case it annotates the following word,
+           or a closed token (other & space), or a unclosed token - in that case it is a right joiner.
+        */
+        if (other)
         {
-          /* it is either after a space, in that case it annotates the following word,
-             or a closed token (other & space), or a unclosed token - in that case it is a right joiner.
-            */
-          if (other)
-          {
-            tokens.back().join_right = true;
-            continue;
-          }
-          else if (space)
-          {
-            token.join_left = true;
-            continue;
-          }
-          else
-          {
-            token.join_right = true;
-            flush_token(tokens, token);
-            state = State::Space;
-            continue;
-          }
-        }
-
-        const unicode::code_point_t v = code_points_main[i];
-        const unicode::CharType char_type = char_types[i];
-        const unicode::CharType next_char_type = (i + 1 < char_types.size()
-                                                  ? char_types[i + 1]
-                                                  : unicode::CharType::Other);
-        const bool is_separator = (char_type == unicode::CharType::Separator
-                                   && code_points_combining[i].empty());
-
-        if (placeholder)
-        {
-          if (v == ph_marker_close_cp)
-          {
-            token.append(c);
-            if (_options.preserve_placeholders)
-              token.preserve = true;
-            prev_alphabet = placeholder_alphabet;
-            state = State::Letter;
-          }
-          else
-          {
-            if (is_separator && !_options.no_substitution)
-              token.append(protected_character + int_to_hex(v));
-            else
-              token.append(c);
-          }
-        }
-        else if (v == ph_marker_open_cp)
-        {
-          if (!space)
-          {
-            flush_token(tokens, token);
-            if ((letter && prev_alphabet != placeholder_alphabet) || number)
-              token.join_left = true;
-            else
-              tokens.back().join_right = true;
-          }
-          else if (other && token.empty())
-            tokens.back().join_right = true;
-          token.append(c);
-          state = State::Placeholder;
-        }
-        else if (is_separator)
-        {
-          if (!space)
-            flush_token(tokens, token);
-
-          if (v == 0x200D) // Zero-Width joiner.
-          {
-            if (other || (number && next_char_type == unicode::CharType::Letter))
-              tokens.back().join_right = true;
-            else
-            {
-              token = Token();
-              token.join_left = true;
-            }
-          }
-          else if (_options.with_separators)
-          {
-            token.append(c);
-            if (next_char_type != unicode::CharType::Separator)
-              flush_token(tokens, token);
-          }
-
-          state = State::Space;
-        }
-        else if (v < 32 || v == 0xFEFF)  // skip special characters and BOM
+          tokens.back().join_right = true;
           continue;
+        }
+        else if (space)
+        {
+          token.join_left = true;
+          continue;
+        }
         else
         {
-          const std::string& sub_c(_options.no_substitution ? c : normalize_character(c, v));
-          bool is_letter = char_type == unicode::CharType::Letter;
-          bool is_number = char_type == unicode::CharType::Number;
-          int alphabet = unicode::get_script(v);
-
-          if (alphabets != nullptr)
-          {
-            const char* alphabet_name = nullptr;
-            if (is_letter && alphabet >= 0)
-              alphabet_name = unicode::get_script_name(alphabet);
-            else if (is_number)
-              alphabet_name = "Numeric";
-            else
-              alphabet_name = "Other";
-
-            (*alphabets)[alphabet_name]++;
-          }
-
-          if (_options.mode == Mode::Conservative)
-          {
-            if (is_number
-                || (sub_c[0] == '-' && letter)
-                || (sub_c[0] == '_')
-                || (letter
-                    && (sub_c[0] == '.' || sub_c[0] == ',')
-                    && (next_char_type == unicode::CharType::Number
-                        || next_char_type == unicode::CharType::Letter)))
-            {
-              is_letter = true;
-              alphabet = number_alphabet;
-            }
-          }
-
-          if (is_letter && _options.mode != Mode::Char)
-          {
-            const unicode::CaseType case_type = unicode::get_case_v2(v);
-            const Casing new_casing = update_casing(token.casing,
-                                                    case_type,
-                                                    token.unicode_length());
-
-            bool segment_case = false;
-            bool segment_alphabet = false;
-            bool segment_alphabet_change = false;
-            if ((!letter && !space)
-                || (letter
-                    && ((!_options.segment_alphabet_codes.empty()
-                         && (segment_alphabet = (
-                               alphabet >= 0
-                               && prev_alphabet == alphabet
-                               && (_options.segment_alphabet_codes.find(alphabet)
-                                   != _options.segment_alphabet_codes.end()))))
-                        || (_options.segment_alphabet_change
-                            && (segment_alphabet_change = (alphabet != prev_alphabet)))
-                        || (prev_alphabet == placeholder_alphabet)
-                        || (_options.segment_case
-                            && (segment_case = (new_casing == Casing::Mixed))))))
-            {
-              token.join_right = true;
-              if (_options.preserve_segmented_tokens
-                  && (segment_case || segment_alphabet || segment_alphabet_change))
-                token.preserve = true;
-              flush_token(tokens, token);
-              token.casing = update_casing(token.casing, case_type, 0);
-            }
-            else
-            {
-              token.casing = new_casing;
-              if (other && token.empty())
-                tokens.back().join_right = true;
-            }
-
-            token.append(sub_c);
-            state = State::Letter;
-            prev_alphabet = alphabet;
-          }
-          else if (is_number && _options.mode != Mode::Char)
-          {
-            const bool segment_number = (_options.segment_numbers && number);
-            if (letter || segment_number || (!number && !space))
-            {
-              if (_options.preserve_segmented_tokens && segment_number)
-                token.preserve = true;
-              flush_token(tokens, token);
-              if (!letter || prev_alphabet == placeholder_alphabet)
-                tokens.back().join_right = true;
-              else
-                token.join_left = true;
-            }
-            else if (other)
-            {
-              tokens.back().join_right = true;
-            }
-
-            token.append(sub_c);
-            state = State::Number;
-          }
-          else
-          {
-            if (!space)
-            {
-              flush_token(tokens, token);
-              token.join_left = true;
-            }
-            else if (other)
-            {
-              token = Token();
-              token.join_left = true;
-            }
-
-            if (sub_c[0] == ' ' && !_options.no_substitution)
-              token.append(protected_character + int_to_hex(sub_c[0]) + sub_c.substr(1));
-            else
-              token.append(sub_c);
-
-            flush_token(tokens, token);
-            state = State::Other | State::Space;
-          }
+          token.join_right = true;
+          flush_token(tokens, token);
+          state = State::Space;
+          continue;
         }
       }
 
-      if (!token.empty())
-        flush_token(tokens, token);
+      const unicode::code_point_t v = code_points_main[i];
+      const unicode::CharType char_type = char_types[i];
+      const unicode::CharType next_char_type = (i + 1 < char_types.size()
+                                                ? char_types[i + 1]
+                                                : unicode::CharType::Other);
+      const bool is_separator = (char_type == unicode::CharType::Separator
+                                 && code_points_combining[i].empty());
+
+      if (placeholder)
+      {
+        if (v == ph_marker_close_cp)
+        {
+          token.append(c);
+          if (_options.preserve_placeholders)
+            token.preserve = true;
+          prev_alphabet = placeholder_alphabet;
+          state = State::Letter;
+        }
+        else
+        {
+          if (is_separator && !_options.no_substitution)
+            token.append(protected_character + int_to_hex(v));
+          else
+            token.append(c);
+        }
+      }
+      else if (v == ph_marker_open_cp)
+      {
+        if (!space)
+        {
+          flush_token(tokens, token);
+          if ((letter && prev_alphabet != placeholder_alphabet) || number)
+            token.join_left = true;
+          else
+            tokens.back().join_right = true;
+        }
+        else if (other && token.empty())
+          tokens.back().join_right = true;
+        token.append(c);
+        state = State::Placeholder;
+      }
+      else if (is_separator)
+      {
+        if (!space)
+          flush_token(tokens, token);
+
+        if (v == 0x200D) // Zero-Width joiner.
+        {
+          if (other || (number && next_char_type == unicode::CharType::Letter))
+            tokens.back().join_right = true;
+          else
+          {
+            token = Token();
+            token.join_left = true;
+          }
+        }
+        else if (_options.with_separators)
+        {
+          token.append(c);
+          if (next_char_type != unicode::CharType::Separator)
+            flush_token(tokens, token);
+        }
+
+        state = State::Space;
+      }
+      else if (v < 32 || v == 0xFEFF)  // skip special characters and BOM
+        continue;
+      else
+      {
+        const std::string& sub_c(_options.no_substitution ? c : normalize_character(c, v));
+        bool is_letter = char_type == unicode::CharType::Letter;
+        bool is_number = char_type == unicode::CharType::Number;
+        int alphabet = unicode::get_script(v);
+
+        if (alphabets != nullptr)
+        {
+          const char* alphabet_name = nullptr;
+          if (is_letter && alphabet >= 0)
+            alphabet_name = unicode::get_script_name(alphabet);
+          else if (is_number)
+            alphabet_name = "Numeric";
+          else
+            alphabet_name = "Other";
+
+          (*alphabets)[alphabet_name]++;
+        }
+
+        if (_options.mode == Mode::Conservative)
+        {
+          if (is_number
+              || (sub_c[0] == '-' && letter)
+              || (sub_c[0] == '_')
+              || (letter
+                  && (sub_c[0] == '.' || sub_c[0] == ',')
+                  && (next_char_type == unicode::CharType::Number
+                      || next_char_type == unicode::CharType::Letter)))
+          {
+            is_letter = true;
+            alphabet = number_alphabet;
+          }
+        }
+
+        if (is_letter && _options.mode != Mode::Char)
+        {
+          const unicode::CaseType case_type = unicode::get_case_v2(v);
+          const Casing new_casing = update_casing(token.casing,
+                                                  case_type,
+                                                  token.unicode_length());
+
+          bool segment_case = false;
+          bool segment_alphabet = false;
+          bool segment_alphabet_change = false;
+          if ((!letter && !space)
+              || (letter
+                  && ((!_options.segment_alphabet_codes.empty()
+                       && (segment_alphabet = (
+                             alphabet >= 0
+                             && prev_alphabet == alphabet
+                             && (_options.segment_alphabet_codes.find(alphabet)
+                                 != _options.segment_alphabet_codes.end()))))
+                      || (_options.segment_alphabet_change
+                          && (segment_alphabet_change = (alphabet != prev_alphabet)))
+                      || (prev_alphabet == placeholder_alphabet)
+                      || (_options.segment_case
+                          && (segment_case = (new_casing == Casing::Mixed))))))
+          {
+            token.join_right = true;
+            if (_options.preserve_segmented_tokens
+                && (segment_case || segment_alphabet || segment_alphabet_change))
+              token.preserve = true;
+            flush_token(tokens, token);
+            token.casing = update_casing(token.casing, case_type, 0);
+          }
+          else
+          {
+            token.casing = new_casing;
+            if (other && token.empty())
+              tokens.back().join_right = true;
+          }
+
+          token.append(sub_c);
+          state = State::Letter;
+          prev_alphabet = alphabet;
+        }
+        else if (is_number && _options.mode != Mode::Char)
+        {
+          const bool segment_number = (_options.segment_numbers && number);
+          if (letter || segment_number || (!number && !space))
+          {
+            if (_options.preserve_segmented_tokens && segment_number)
+              token.preserve = true;
+            flush_token(tokens, token);
+            if (!letter || prev_alphabet == placeholder_alphabet)
+              tokens.back().join_right = true;
+            else
+              token.join_left = true;
+          }
+          else if (other)
+          {
+            tokens.back().join_right = true;
+          }
+
+          token.append(sub_c);
+          state = State::Number;
+        }
+        else
+        {
+          if (!space)
+          {
+            flush_token(tokens, token);
+            token.join_left = true;
+          }
+          else if (other)
+          {
+            token = Token();
+            token.join_left = true;
+          }
+
+          if (sub_c[0] == ' ' && !_options.no_substitution)
+            token.append(protected_character + int_to_hex(sub_c[0]) + sub_c.substr(1));
+          else
+            token.append(sub_c);
+
+          flush_token(tokens, token);
+          state = State::Other | State::Space;
+        }
+      }
     }
+
+    if (!token.empty())
+      flush_token(tokens, token);
   }
 
   static inline void add_final_token(std::vector<std::string>& tokens,
